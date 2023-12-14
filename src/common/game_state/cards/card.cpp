fbe9c129cb8113f3f73d795ef0d66c6ab83ef2d8
Samuel Relling <srelling@student.ethz.ch>
2023-12-14 15:50:59 +0100

    Fixed swapping and Card.tostring

--- src/common/game_state/cards/card.cpp
+++ src/common/game_state/cards/card.cpp
@@ -33,41 +33,60 @@ std::string Card::to_string() const {
     switch(_suit) {
         case GREEN:
             card_string += "Green";
+            break;
         case RED:
             card_string += "Red";
+            break;
         case BLUE:
             card_string += "Blue";
+            break;
         case SCHWARZ:
             card_string += "Black";
+            break;
     }
+
+    card_string += " ";
 
     switch(_rank) {
         case TWO:
             card_string += "Two";
+            break;
         case THREE:
             card_string += "Three";
+            break;
         case FOUR:
             card_string += "Four";
+            break;
         case FIVE:
             card_string += "Five";
+            break;
         case SIX:
             card_string += "Six";
+            break;
         case SEVEN:
             card_string += "Seven";
+            break;
         case EIGHT:
             card_string += "Eight";
+            break;
         case NINE: 
             card_string += "Nine";
+            break;
         case TEN:
             card_string += "Ten";
+            break;
         case JACK:
             card_string += "Jack";
+            break;
         case QUEEN:
             card_string += "Queen";
+            break;
         case KING:
             card_string += "King";
+            break;
         case ACE:
             card_string += "Ace";
+            break;
     }
     card_string += "]";
     return card_string;
