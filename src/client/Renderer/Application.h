62dd44ee959e832e835f9f3015552842fa7cd7f3
Alexander Rohwedder <arohwedder@ethz.ch>
2023-12-17 15:38:48 +0100

    added Doxygen comments to Application.h

--- src/client/Renderer/Application.h
+++ src/client/Renderer/Application.h
@@ -1,3 +1,11 @@
+/*! \class Application
+    \brief Generates and manages the application framework.
+
+ This is the core of the application framework, managing window creation, layer management, and providing utility functions
+ to access information about the application state. \n
+ Only a single instance is allowed.
+*/
+
 #ifndef TICHU_APPLICATION_H
 #define TICHU_APPLICATION_H
 
@@ -11,7 +19,7 @@
 
 class Window;
 
-// helper class for time
+//! helper class for time measurement, representing the duration of time elapsed
 class TimeStep {
 private:
     float m_Time;
@@ -28,32 +36,29 @@ public:
 };
 
 
-// interface for running custom code in the application
+//! Interface for running custom code in the application
 class Layer {
 public:
 
     virtual ~Layer() = default;
 
-    /**
-     * called when pushing the layer to the app
-     */ 
+
+    //! Called when pushing the layer to the app
     virtual void on_attach() {};
 
-    /**
-     * called in the Application destructor
-     */ 
+
+    //! Called in the Application destructor
     virtual void on_detach() {};
 
-    /**
-     * can help separating gui and render code
-    */ 
+
+    //! Can help separating gui and render code
     virtual void on_update(TimeStep ts) {};
 
     virtual void on_imgui() {};
 };
 
 
-// helper struct for creating an app
+//! Helper struct for creating an application
 struct ApplicationCreateInfo {
     std::string title = "Application";
     glm::uvec2 size {100, 100};
@@ -71,30 +76,30 @@ public:
 
     ~Application();
 
-    // runs the app
+    //! Runs the applicaton
     void run();
 
-    // used to update the application when e.g the window is being resized
+    //! Updates the application when the window is being resized
     static void update_frame();
 
     void push_layer(const std::shared_ptr<Layer> &layer);
 
-    // returns the aspect ratio of the viewport
+    //! Returns the aspect ratio of the viewport
     [[nodiscard]] static float get_aspect_ratio() {
         const auto size = get_viewport_size();
         return (float) size.x / (float) size.y;
     }
 
-    // returns the size of the viewport window
+    //! Returns the size of the viewport window
     [[nodiscard]] static const glm::uvec2 &get_viewport_size() { return get_instance()->_viewport_size; }
     [[nodiscard]] static const glm::uvec2 &get_viewport_pos() { return get_instance()->_viewport_pos;}
 
-    // returns the size of the window
+    //! Returns the size of the window
     [[nodiscard]] static const glm::uvec2 &get_window_size() { return get_instance()->_window_size; }
 
     [[nodiscard]] static glm::vec2 get_mouse_pos();
 
-    // returns the application instance
+    //! Returns the application instance
     [[nodiscard]] static const Application *get_instance();
 
     [[nodiscard]] static const ApplicationCreateInfo &get_info() { return get_instance()->_info; };
