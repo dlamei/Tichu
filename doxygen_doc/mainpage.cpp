8c0509d6a85fdccad4204db2b1169594ad6c1758
tinaho <tinaho@student.ethz.ch>
2023-12-17 22:10:40 +0100

    changed mainpage of doxygen

--- doxygen_doc/mainpage.cpp
+++ doxygen_doc/mainpage.cpp
@@ -1,3 +1,11 @@
 /*! \mainpage Tichu - Team SALT'D
+* This is a simple C++ implementation of the game "Tichu". The official game is published by Fata Morgana, 
+* you can find the official [German rules here](https://abacusspiele.de/wp-content/uploads/2021/01/Tichu_Regel.pdf) 
+* and the [English ones here](https://fatamorgana.ch/fatamorgana/tichu/english-rules).
+* The implementation features a client/server architecture for multiplayer scenarios.
+* It uses [Dear ImGui](https://github.com/ocornut/imgui) for the GUI, [sockpp](https://github.com/fpagliughi/sockpp) 
+* for the network interface, [nlohmann/json](https://github.com/nlohmann/json) for object serialization, 
+* and [googletest](https://github.com/google/googletest) for the unit tests.
+*
 * https://gitlab.ethz.ch/srelling/tichu-project
 */
